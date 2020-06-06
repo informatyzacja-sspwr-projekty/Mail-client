f49c4b3e60261f016fd915ad908cf5de4df0a2ab
Byczax <byczqq136@gmail.com>
2020-06-06 21:05:36 +0200

    Custom message
    Signed-off-by: Byczax <byczqq136@gmail.com>

--- src/send_mails.py
+++ src/send_mails.py
@@ -16,23 +16,36 @@ def read_json(filename: str):
     return data
 
 
+def message_content(mail_data, uuid, user):
+    filename = mail_data["Message_content"]
+    file = open(filename, "rt")
+    message = file.read()
+    message.replace("<user>", user)
+    message.replace("<link>", ("<part 1 link>" + uuid))
+    return message
+
+
 # todo potem zmienić msg_content na listę stringów?
 #  bo dla każdego odbiorcy chyba trzeba osobną treść
-def send_mails(mail_data, receivers, subject: str, msg_content: str):
+def send_mails(mail_data, receivers):
     mail_user = mail_data["Mail"]
     mail_password = mail_data["Password"]
+
     with smtplib.SMTP(mail_data["Host"], mail_data["Port"]) as sending_mail:
         sending_mail.starttls()  # https://docs.python.org/3/library/smtplib.html#smtplib.SMTP.starttls
         sending_mail.login(mail_user, mail_password)
+
         for receiver in receivers:
             message = EmailMessage()
-            message['Subject'] = subject
+            message['Subject'] = mail_data["Subject"]
+            msg_content = message_content(mail_data, receiver.uuid, receiver.name)
             message.set_content(msg_content)
-            sending_mail.sendmail(mail_user, receiver.mail, message.as_string())
+            print(msg_content)
+            # sending_mail.sendmail(mail_user, receiver.mail, message.as_string())
 
 
 if __name__ == "__main__":
     mail_dataa = read_json("../mail_login.json")[0]  # [0] to get the dict
-    user_data = read_json("../Conversion_results.json")  # mail receivers json file
+    user_data = read_json("Conversion_results.json")  # mail receivers json file
     mail_receivers = map(lambda x: MailReceiver(x), user_data)
-    send_mails(mail_dataa, mail_receivers, "TEMAT", "WIADOMOSC")
+    send_mails(mail_dataa, mail_receivers)
