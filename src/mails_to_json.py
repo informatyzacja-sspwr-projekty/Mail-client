5b39d7bb4deb2a8fc6c3394e9970bc682e8d51e6
FilipJQ77 <filipprzygonski99@gmail.com>
2020-06-20 00:46:26 +0200

    documented code, added writing logs to separate files

--- src/mails_to_json.py
+++ src/mails_to_json.py
@@ -1,9 +1,16 @@
 import json
 import re
 import uuid
+import datetime
+
+
+def current_time() -> str:
+    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
 def convert_file_to_dict_list(mail_data_dict):
+    """Converts a file with users and email addresses given in mail_data.json into a list of dictionaries,
+    where each dictionary is a separate email."""
     filename = mail_data_dict["MailsTxtFile"]
     username = None
     user_email = None
@@ -24,16 +31,19 @@ def convert_file_to_dict_list(mail_data_dict):
 
 
 def convert_dict_list_to_json(dict_list, filename: str):
+    """Converts a list of dictionaries into a JSON file."""
     with open(filename, "w") as file:
         json.dump(dict_list, file, indent=2)
 
 
 def convert_file_to_json(filename, json_filename):
+    """Converts a file with users and email addresses into a JSON file."""
     dictionary_list = convert_file_to_dict_list(filename)
     convert_dict_list_to_json(dictionary_list, json_filename)
 
 
 def read_json(filename: str):
+    """Reads a JSON file and returns it as a list of dictionaries."""
     with open(filename) as file:
         data = json.load(file)
     return data
@@ -43,3 +53,5 @@ if __name__ == "__main__":
     mail_dict = read_json("../mail_data.json")[0]
     convert_file_to_json(mail_dict, mail_dict["MailsJsonFile"])
     print("Converted!")
+    with open("../log.txt", 'a') as log_file:
+        log_file.write(f"{current_time()} Mails converted")
--- src/send_mails.py
+++ src/send_mails.py
@@ -1,10 +1,17 @@
 import time
 import smtplib
 import json
+import datetime
 from email.message import EmailMessage
 
 
+def current_time() -> str:
+    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
 class MailReceiver:
+    """Class made to simplify handling mail receivers."""
+
     def __init__(self, receiver_properties: dict):
         self.name = receiver_properties["Name"]
         self.mail = receiver_properties["Mail"]
@@ -12,18 +19,21 @@ class MailReceiver:
 
 
 def read_json(filename: str):
+    """Reads a JSON file and returns it as a list of dictionaries."""
     with open(filename) as file:
         data = json.load(file)
     return data
 
 
-def generate_message_content(user, uuid, link, message):
+def message_replace(user, uuid, link, message):
+    """Replace tags in message with given information."""
     message = message.replace("{user}", user)
     message = message.replace("{link}", (link + uuid))
     return message
 
 
 def send_mails(mail_data_dict, receivers, link):
+    """Sends mails to given receivers, with a given link."""
     mail_user = mail_data_dict["Mail"]
     mail_password = mail_data_dict["Password"]
     with open(mail_data_dict["MessageContentFile"], encoding="utf-8", mode="rt") as file:
@@ -31,17 +41,27 @@ def send_mails(mail_data_dict, receivers, link):
     with smtplib.SMTP(mail_data_dict["Host"], mail_data_dict["Port"]) as sending_mail:
         sending_mail.starttls()  # https://docs.python.org/3/library/smtplib.html#smtplib.SMTP.starttls
         sending_mail.login(mail_user, mail_password)
+        log_file = open("../log.txt", 'a')
+        sent_file = open("../sent.txt", 'w')
+        notsent_file = open("../notsent.txt", 'w')
         for receiver in receivers:
             message = EmailMessage()
             message['Subject'] = mail_data_dict["Subject"]
-            content = generate_message_content(receiver.name, receiver.uuid, link, message_content)
+            content = message_replace(receiver.name, receiver.uuid, link, message_content)
             message.set_content(content)
             time.sleep(10)
             try:
                 sending_mail.sendmail(mail_user, receiver.mail, message.as_string())
                 print(f"{receiver.mail} sent")
+                log_file.write(f"{current_time()} {receiver.mail} sent")
+                sent_file.write(f"{receiver.mail}")
             except Exception as e:
                 print(f"{receiver.mail} not sent, exception: {e}")
+                log_file.write(f"{current_time()} {receiver.mail} not sent, reason: {e}")
+                notsent_file.write(f"{receiver.mail}")
+        log_file.close()
+        sent_file.close()
+        notsent_file.close()
 
 
 if __name__ == "__main__":
