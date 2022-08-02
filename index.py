17a759351ac7bdb5d0935890e00b816387ef4f45
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-08-02 23:22:38 +0200

    Uwzględnienie logów przy wysyłce (#11)
    * Checking logs before sending
    
    * Fixed logs
    
    * Code refactoring
    
    * Moved code outside of statement
    
    * Code formatting

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from os.path import exists
 from src import mail_sender, mails_to_json, utils
 from src.mail_receiver import MailReceiver
 
@@ -17,10 +18,26 @@ def send_mails(config: dict):
     """Prepares users' data and sends mails to receivers."""
 
     users_data = utils.read_json(f"data/{config['mails_json_file']}")
+
+    if exists(f"logs/sent.log"):
+        all_mails = [sub['mail'] for sub in users_data]
+
+        with open(f"logs/sent.log") as file:
+            log_mails = file.readlines()
+            sent_mails = [line.rstrip() for line in log_mails]
+            diff = list(set(all_mails) - set(sent_mails))
+
+            if diff:
+                users_data = [x for x in users_data if x['mail'] in diff]
+
+            else:
+                print("No new mails have been sent!")
+                utils.log(f"{utils.current_time()} No new mails sent")
+
+                return
+
     mail_receivers = map(lambda x: MailReceiver(x), users_data)
-
     mail_sender.send_mails(config, mail_receivers)
-
     print("Mails have been sent!")
     utils.log(f"{utils.current_time()} Mails sent")
 
@@ -38,7 +55,7 @@ if __name__ == "__main__":
     config = utils.load_config()
 
     utils.setup_dirs()
-    utils.clear_logs()
+    #utils.clear_logs()
 
     convert_mails(config["mails_txt_file"], config["mails_json_file"])
     send_mails(config)
