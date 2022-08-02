a9f5f8673a386051760325feef43a248f404e4d3
Damian Ryś <damian.rys0@gmail.com>
2022-08-02 21:19:39 +0200

    Code formatting

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+from os.path import exists
 from src import mail_sender, mails_to_json, utils
 from src.mail_receiver import MailReceiver
-from os.path import exists
 
 
 def convert_mails(txt_filename: str, json_filename: str):
@@ -18,18 +18,24 @@ def send_mails(config: dict):
     """Prepares users' data and sends mails to receivers."""
 
     users_data = utils.read_json(f"data/{config['mails_json_file']}")
+
     if exists(f"logs/sent.log"):
         all_mails = [sub['mail'] for sub in users_data]
+
         with open(f"logs/sent.log") as file:
             log_mails = file.readlines()
             sent_mails = [line.rstrip() for line in log_mails]
             diff = list(set(all_mails) - set(sent_mails))
+
             if diff:
                 users_data = [x for x in users_data if x['mail'] in diff]
+
             else:
                 print("No new mails have been sent!")
                 utils.log(f"{utils.current_time()} No new mails sent")
+
                 return
+
     mail_receivers = map(lambda x: MailReceiver(x), users_data)
     mail_sender.send_mails(config, mail_receivers)
     print("Mails have been sent!")
