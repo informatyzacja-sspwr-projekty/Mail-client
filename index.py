dff079614b609d284b0e030637cb61bef76f4024
Damian Ryś <damian.rys0@gmail.com>
2022-08-02 07:18:59 +0200

    Code refactoring

--- index.py
+++ index.py
@@ -26,14 +26,10 @@ def send_mails(config: dict):
             diff = list(set(all_mails) - set(sent_mails))
             if diff:
                 users_data = [x for x in users_data if x['mail'] in diff]
-                mail_receivers = map(lambda x: MailReceiver(x), users_data)
-                mail_sender.send_mails(config, mail_receivers)
-                print("Mails have been sent!")
-                utils.log(f"{utils.current_time()} Mails sent")
             else:
                 print("No new mails have been sent!")
                 utils.log(f"{utils.current_time()} No new mails sent")
-    else:
+                return
         mail_receivers = map(lambda x: MailReceiver(x), users_data)
         mail_sender.send_mails(config, mail_receivers)
         print("Mails have been sent!")
