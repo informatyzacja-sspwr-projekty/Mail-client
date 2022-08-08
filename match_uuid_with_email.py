34f42129150b6082a100cbfac6f94cfc29392625
enth24 <266855@student.pwr.edu.pl>
2022-08-08 20:53:40 +0200

    with open multiple files, removed sort(), no uuid repetitions

--- match_uuid_with_email.py
+++ match_uuid_with_email.py
@@ -1,20 +1,11 @@
 import json
 
-emails_file = open("mails.json", "r")
-uuids_file = open("uuid.txt", "r")
-emails = open("emails.txt", "w")
+with open("mails.json", "r") as emails_file, open("uuid.txt", "r") as uuids_file, open("emails.txt", "w") as emails:
+    email_uuid_json = json.load(emails_file)
+    uuid_lines = list(set([el.replace('\n', '') for el in uuids_file.readlines()]))
+    email_uuid_lines = [record['uuid'] for record in email_uuid_json]
 
-email_uuid_json = json.load(emails_file)
-uuid_lines = [el.replace('\n', '') for el in uuids_file.readlines()]
-email_uuid_lines = [record['uuid'] for record in email_uuid_json]
-
-uuid_lines.sort()
-email_uuid_lines.sort()
-
-for record in email_uuid_json:
     for uuid in email_uuid_lines:
-        if record['uuid'] == uuid:
-            emails.write(record['mail'] + '\n')
-
-emails_file.close()
-uuids_file.close()
+        for record in email_uuid_json:
+            if record['uuid'] == uuid:
+                emails.write(record['mail'] + '\n')
