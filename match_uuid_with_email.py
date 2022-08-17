f19b99c0fc24d27bd1dc2fd97dcfaf6edcbc0c01
Omikorin <michal.k2001@gmail.com>
2022-08-18 01:19:17 +0200

    refactor match script

--- match_uuid_with_email.py
+++ match_uuid_with_email.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
 import json
 
-with open("mails.json", "r") as emails_file, open("uuid.txt", "r") as uuids_file, open("emails.txt", "w") as emails:
-    email_uuid_json = json.load(emails_file)
-    uuid_lines = list(set([el.replace('\n', '') for el in uuids_file.readlines()]))
-    email_uuid_lines = [record['uuid'] for record in email_uuid_json]
 
-    for uuid in uuid_lines:
-        for record in email_uuid_json:
-            if record['uuid'] == uuid:
-                emails.write(record['mail'] + '\n')
+def main():
+    with open("mails.json", "r") as emails_file, open("uuid.txt", "r") as uuids_file, open("confirmed_emails.txt", "w") as confirmed_emails:
+        email_json = json.load(emails_file)
+
+        uuids = list(set([el.replace('\n', '')
+                     for el in uuids_file.readlines()]))
+
+        for uuid in uuids:
+            for record in email_json:
+                if record['uuid'] == uuid:
+                    confirmed_emails.write(record['mail'] + '\n')
+
+
+if __name__ == "__main__":
+    main()
